92b1fbe51784a4aee953dc32db48239b1d4c2bf2
arthur <bitbegin@gmail.com>
2014-04-26 20:20:23 +0800

    add buttons test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,27 +3,30 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
-	  $scope.oneAtATime = true;
-
-	  $scope.groups = [
-	    {
-	      title: "Dynamic Group Header - 1",
-	      content: "Dynamic Group Body - 1"
-	    },
-	    {
-	      title: "Dynamic Group Header - 2",
-	      content: "Dynamic Group Body - 2"
-	    }
+  .controller('MyCtrl1', ['$scope', function($scope) {
+	  $scope.alerts = [
+	    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+	    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
 	  ];
 
-	  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+	  $scope.addAlert = function() {
+	    $scope.alerts.push({msg: "Another alert!"});
+	  };
 
-	  $scope.addItem = function() {
-	    var newItemNo = $scope.items.length + 1;
-	    $scope.items.push('Item ' + newItemNo);
+	  $scope.closeAlert = function(index) {
+	    $scope.alerts.splice(index, 1);
 	  };
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+	  $scope.singleModel = 1;
+
+	  $scope.radioModel = 'Middle';
+
+	  $scope.checkModel = {
+	    left: false,
+	    middle: true,
+	    right: false
+	  };
 
   }]);
