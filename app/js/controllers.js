0a60f10983b3dec09a4b90653f41c14382228c8b
arthur <bitbegin@gmail.com>
2014-04-26 17:49:23 +0800

    add simple test in partial1.html with bootstrap ui

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,25 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', [function() {
+	  $scope.oneAtATime = true;
 
+	  $scope.groups = [
+	    {
+	      title: "Dynamic Group Header - 1",
+	      content: "Dynamic Group Body - 1"
+	    },
+	    {
+	      title: "Dynamic Group Header - 2",
+	      content: "Dynamic Group Body - 2"
+	    }
+	  ];
+
+	  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+	  $scope.addItem = function() {
+	    var newItemNo = $scope.items.length + 1;
+	    $scope.items.push('Item ' + newItemNo);
+	  };
   }])
   .controller('MyCtrl2', [function() {
 
