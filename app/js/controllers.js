2ace444f4423dd58341873a4efeb088595efe8d6
arthur <bitbegin@gmail.com>
2014-05-11 14:48:32 +0800

    修改布局

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,8 +6,6 @@ angular.module('myApp.controllers', [])
   .controller('MainCtrl', ['$scope', '$location', '$document', function($scope, $location, $document) {
     $scope.items = [
       {path: '/home', title: '首页'},
-      {path: '/photos', title: '相册'},
-      {path: '/spaces', title: '太空科幻'},
       {path: '/about', title: '关于'},
     ];
     $scope.top = "";
@@ -24,50 +22,12 @@ angular.module('myApp.controllers', [])
 
   }])
 
-  .controller('MainPhotoCtrl', ['$scope', function($scope) {
-    $scope.myInterval = 5000;
-    $scope.slides = [
-      {image: 'http://dofa.u.qiniudn.com/images/background/bower.jpg'},
-      {image: 'http://dofa.u.qiniudn.com/images/background/ship.jpg'},
-      {image: 'http://dofa.u.qiniudn.com/images/background/sits.jpg'},
-      // {image: 'http://dofa.u.qiniudn.com/images/background/Summer-Country-Road-1366x768-desktopia.net.jpg'},
-      // {image: 'http://dofa.u.qiniudn.com/images/background/Mountain-Road1-1366x768-desktopia.net.jpg'},
-      // {image: 'http://dofa.u.qiniudn.com/images/background/monty_python_ministry_of_silly_walks_desktop_1920x1200_hd-wallpaper-826419.jpg'},
-    ];      
-  }])
+
   .controller('HomeCtrl', ['$scope', function($scope) {
 
   
   }])
 
-  .controller('PhotosCtrl', ['$scope', function($scope) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-      var newWidth = 600 + slides.length;
-      slides.push({
-        image: 'http://placekitten.com/' + newWidth + '/300',
-        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-      });
-    };
-    for (var i=0; i<4; i++) {
-      $scope.addSlide();
-    }
-  }])
-
-  .controller('SpacesCtrl', ['$scope', function($scope) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function(index) {
-      slides.push({
-        image: 'http://dofa.u.qiniudn.com/image/space/太空科幻' + index + '.jpg'
-      });
-    };
-    for (var i=1; i<26; i++) {
-      $scope.addSlide(i);
-    }
-  }])
 
   .controller('AboutCtrl', ['$scope', function($scope) {
 
